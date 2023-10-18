485e419cdf6d8f01d2f5b9fd727df4d758ed4a03
Alejandro <123700084+recheitor@users.noreply.github.com>
2023-10-18 17:03:19 +0200

    finished homepage

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,22 +1,15 @@
 import { Col, Row, } from 'react-bootstrap'
-import { Link } from 'react-router-dom'
 import './Navigation.css'
-import { useState } from 'react'
 
 const Navigation = () => {
 
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
-
-    const closeMenu = () => {
-        setIsMenuOpen(false);
-    };
 
     return (
         <>
-            <Row className='navi justify-content-between align-items-center'>
-                <Col xs='2' lg='3' >
-                    <div className='nav-my-name text-center'>
-                        <a href="#intro-blank-section"> <h3>IRENE BUCETA</h3></a>
+            <Row className='navi align-items-center'>
+                <Col xs='6' lg='3' >
+                    <div className='nav-my-name'>
+                        <img src="./../../img/LOGO.png" alt="" />
                     </div>
                 </Col>
             </Row >
