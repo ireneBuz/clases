d9d0f656c28d946af9899f641896f915346d5ac4
Alejandro <123700084+recheitor@users.noreply.github.com>
2023-10-18 16:34:42 +0200

    finally working img

--- src/App.jsx
+++ src/App.jsx
@@ -10,9 +10,6 @@ function App() {
   return (
     <>
 
-      <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" />
-      <link href="https://fonts.googleapis.com/css2?family=Lexend:wght@300;500;800&display=swap" rel="stylesheet" />
       <Navigation />
       <AppRoutes />
       {/* <Footer /> */}
