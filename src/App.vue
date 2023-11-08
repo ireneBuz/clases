0e16d7103cb5d9a5495aa27f41f0cfcd757fa329
Alejandro <123700084+recheitor@users.noreply.github.com>
2023-11-08 16:13:10 +0100

    homepage almost

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <script setup>
 import Navigation from './components/Navigation.vue';
 import HomePageView from './views/HomePageView.vue';
+import Footer from './components/Footer.vue';
+
 </script>
 
 <template>
   <header>
     <Navigation />
     <HomePageView />
+    <Footer />
   </header>
 </template>
 
